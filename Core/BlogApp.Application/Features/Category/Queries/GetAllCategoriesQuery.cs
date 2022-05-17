cc9e025e3ccfbce905f70e6a711d793f677bf5b6
Mehmet Hamzakadı <mehmethamzakadi@hotmail.com>
2022-05-17 11:12:29 +0300

    CQRS klasör ve dosya yapısı düzenlemeleri.

--- Core/BlogApp.Application/Features/Category/Queries/GetAllCategoriesQuery.cs
+++ Core/BlogApp.Application/Features/Category/Queries/GetAllCategoriesQuery.cs
@@ -1,4 +1,6 @@
-﻿using BlogApp.Application.DTOs.Category;
+﻿using AutoMapper;
+using BlogApp.Application.DTOs.Category;
+using BlogApp.Application.Interfaces.Persistence;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,23 @@ namespace BlogApp.Application.Features.Category.Queries
 {
     public class GetAllCategoriesQuery : IRequest<IReadOnlyList<RsCategory>>
     {
+        public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuery, IReadOnlyList<RsCategory>>
+        {
+            private readonly IUnitOfWork _unitOfWork;
+            private readonly IMapper _mapper;
 
+            public GetAllCategoriesQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+            {
+                _unitOfWork = unitOfWork;
+                _mapper = mapper;
+            }
+
+            public async Task<IReadOnlyList<RsCategory>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
+            {
+                var categories = await _unitOfWork.CategoryRepository.GetAllAsync();
+                var result = _mapper.Map<IReadOnlyList<RsCategory>>(categories);
+                return result;
+            }
+        }
     }
 }
--- Core/BlogApp.Application/Features/Category/Queries/GetByIdCategoryQuery.cs
+++ Core/BlogApp.Application/Features/Category/Queries/GetByIdCategoryQuery.cs
@@ -1,4 +1,6 @@
-﻿using BlogApp.Application.DTOs.Category;
+﻿using AutoMapper;
+using BlogApp.Application.DTOs.Category;
+using BlogApp.Application.Interfaces.Persistence;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -11,5 +13,23 @@ namespace BlogApp.Application.Features.Category.Queries
     public class GetByIdCategoryQuery : IRequest<RsCategory>
     {
         public int Id { get; set; }
+
+        public class GetCategoryByIdQueryHandler : IRequestHandler<GetByIdCategoryQuery, RsCategory>
+        {
+            private readonly IUnitOfWork _unitOfWork;
+            private readonly IMapper _mapper;
+
+            public GetCategoryByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+            {
+                _unitOfWork = unitOfWork;
+                _mapper = mapper;
+            }
+
+            public async Task<RsCategory> Handle(GetByIdCategoryQuery request, CancellationToken cancellationToken)
+            {
+                var category = await _unitOfWork.CategoryRepository.GetByIdAsync(request.Id);
+                return _mapper.Map<RsCategory>(category);
+            }
+        }
     }
 }
