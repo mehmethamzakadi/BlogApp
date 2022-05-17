b6ca1155f50caedf2df8fda4f433d91b22f654df
Mehmet Hamzakadı <mehmethamzakadi@hotmail.com>
2022-05-17 15:59:30 +0300

    Identity geliştirmeleri

---
+++ Core/BlogApp.Application/Features/AppUserFeature/Queries/GetAllAppUsersQuery.cs
@@ -0,0 +1,24 @@
+﻿using BlogApp.Application.DTOs.Common;
+using BlogApp.Application.DTOs.Results;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Features.AppUserFeature.Queries
+{
+    public class GetAllAppUsersQuery : IRequest<BaseResult<RsAppUser>>
+    {
+        //public class GetAllAppUsersQueryHandler : IRequestHandler<GetAllAppUsersQuery, BaseResult<RsAppUser>>
+        //{
+
+        //    public Task<BaseResult<RsAppUser>> Handle(GetAllAppUsersQuery request, CancellationToken cancellationToken)
+        //    {
+                
+        //    }
+        //}
+    }
+}
---
+++ Core/BlogApp.Application/Profiles/AppUserProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BlogApp.Application.DTOs.Params;
+using BlogApp.Application.DTOs.Results;
+using BlogApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Application.Profiles
+{
+    public class AppUserProfile : Profile
+    {
+        public AppUserProfile()
+        {
+            CreateMap<AppUser, RsAppUser>().ReverseMap();
+            CreateMap<AppUser, PmAppUser>().ReverseMap();
+        }
+    }
+}
