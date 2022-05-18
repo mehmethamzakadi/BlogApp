04a27608e54051dcc8c94f2cd9fcb326352bb718
Mehmet Hamzakadı <mehmethamzakadi@hotmail.com>
2022-05-18 13:37:20 +0300

    Düzenleme

--- Core/BlogApp.Application/DTOs/AppUserCreateDto.cs
+++ Core/BlogApp.Application/DTOs/AppUserCreateDto.cs
@@ -9,7 +9,6 @@ namespace BlogApp.Application.DTOs
     public class AppUserCreateDto
     {
         public string UserName { get; set; }
-
         public string Email { get; set; }
         public string Password { get; set; }
     }
--- Core/BlogApp.Application/DTOs/AppUserDto.cs
+++ Core/BlogApp.Application/DTOs/AppUserDto.cs
@@ -9,7 +9,6 @@ namespace BlogApp.Application.DTOs
 {
     public class AppUserDto : BaseDto
     {
-
         public string UserName { get; set; }
 
         public string Email { get; set; }
