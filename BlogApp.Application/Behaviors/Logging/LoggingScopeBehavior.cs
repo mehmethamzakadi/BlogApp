8e77c87446256646b0b608335b6dfb6ba499ab42
Mehmet Hamzakadı <mehmethamzakadi@hotmail.com>
2023-12-17 20:07:06 +0300

    loglama

---
+++ BlogApp.Application/Behaviors/Logging/LoggingScopeBehavior.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Serilog;
+
+namespace BlogApp.Application.Behaviors.Logging
+{
+    public class LoggingScopeBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Log.Information($"Starting request {typeof(TRequest).Name}");
+                var result = await next();
+                Log.Information($"Competed request {typeof(TRequest).Name}");
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, $"Error request {typeof(TRequest).Name}, Message:{ex.Message}");
+                throw;
+            }
+        }
+    }
+}
