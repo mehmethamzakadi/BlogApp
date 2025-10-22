f807b000bfabd7ad05eb96c7269ebf4277ed0f40
Mehmet Hamzakadı <mehmethamzakadi@hotmail.com>
2025-10-22 15:06:31 +0300

    Update BaseApiController routing and introduce IResult alias
    Modified the `[Route]` attribute in `BaseApiController` to include the action name (`api/[controller]/[action]`) for improved routing clarity and flexibility.
    
    Introduced an `IResult` alias for `BlogApp.Domain.Common.Results.IResult` to simplify code references.
    
    Ensured `IMediator` dependency injection in the constructor with a null check. Retained existing `[Authorize]`, `[ApiController]`, and `[ApiExplorerSettings(IgnoreApi = true)]` attributes.

--- src/BlogApp.API/Controllers/BaseApiController.cs
+++ src/BlogApp.API/Controllers/BaseApiController.cs
@@ -9,7 +9,7 @@ namespace BlogApp.API.Controllers
 {
     [Authorize]
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public abstract class BaseApiController(IMediator mediator) : ControllerBase
     {
         protected IMediator Mediator { get; } = mediator ?? throw new ArgumentNullException(nameof(mediator));
